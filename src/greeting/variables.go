bfbea81b14caad14da26847f781195fe328cc656
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-03 15:40:17 +0430

    Fix: a little change.

--- src/greeting/variables.go
+++ src/greeting/variables.go
@@ -1,5 +1,5 @@
 package greeting
 
-// first character of variable shoud be used in Capital form in order to import in other files.
+// first character of variable should be used capital form in order to import in other files.
 
 var GreetingText = "Hello World!!"
