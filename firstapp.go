88e949bb379613a31cbe0b84b7426426c461d5d9
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-03 11:00:35 +0430

    Fix: Declaring Float.

--- firstapp.go
+++ firstapp.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	luckyNumber := 17696969
+	luckyNumber := 17
+	// diffrence between declaration in two ways
+	// if the right side is INT the left would be INT unless it has decimal
+	newNumber := luckyNumber / 3
+	var newNumber1 float64 = float64(luckyNumber) / 3
 
-	var newNumber float64 = float64(luckyNumber) / 3
 	fmt.Println("Hello World!")
 	fmt.Println(luckyNumber)
 	fmt.Println(newNumber)
-
+	fmt.Println(newNumber1)
 }
