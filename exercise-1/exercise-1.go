3b07ba8011d3349eb1a980a0058508ed6c12ccbc
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-03 10:52:59 +0430

    Feat: Complete Exercise1.

---
+++ exercise-1/exercise-1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Amin"
+	lastName := "Mortezaie"
+	currentYear := 2022
+	birthYear := 1998
+	age := currentYear - birthYear
+
+	nextYear := currentYear + 1
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+	fmt.Println(nextYear)
+}
