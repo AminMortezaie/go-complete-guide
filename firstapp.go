6fbb7a189a1fa80b5bf88f896bb099c2d459c544
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-03 10:58:08 +0430

    Fix: Using float64.

--- firstapp.go
+++ firstapp.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	luckyNumber := 17696969
+
+	var newNumber float64 = float64(luckyNumber) / 3
 	fmt.Println("Hello World!")
+	fmt.Println(luckyNumber)
+	fmt.Println(newNumber)
+
 }
