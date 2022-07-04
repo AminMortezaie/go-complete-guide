39f957593ec49f8b5b18353727a8f6fc50e9ebdd
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-04 09:35:02 +0430

    Fix: Bmi app is completed.

--- bmi/bmi.go
+++ bmi/bmi.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
-	fmt.Println("Hello World!!")
+	fmt.Println("BMI Calculator")
+	fmt.Println("---------------")
+	fmt.Print("Please enter your weight(kg): ")
+
+	// underscore means: we don't care about second output.
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Please enter your height(m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	if runtime.GOOS == "windows" {
+		weightInput = strings.Replace(weightInput, "\r\n", "", -1)
+		heightInput = strings.Replace(heightInput, "\r\n", "", -1)
+	} else {
+		weightInput = strings.Replace(weightInput, "\n", "", -1)
+		heightInput = strings.Replace(heightInput, "\n", "", -1)
+	}
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	bmi := weight / (height * height)
+
+	fmt.Printf("Your BMI is: %.2f", bmi)
 }
