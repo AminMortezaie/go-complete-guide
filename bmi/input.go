97ecb102f8ed720a0ea7905dfca5bdfda086e879
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-04 12:38:04 +0430

    Fix: Clean getUsersMetrics.

--- bmi/input.go
+++ bmi/input.go
@@ -15,22 +15,21 @@ var reader = bufio.NewReader(os.Stdin)
 
 func getUsersMetrics() (weight float64, height float64) {
 
-	fmt.Print(info.WeightPrompt)
-	// underscore means: we don't care about second output.
-	weightInput, _ := reader.ReadString('\n')
-
-	fmt.Print(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
-
-	if runtime.GOOS == "windows" {
-		weightInput = strings.Replace(weightInput, "\r\n", "", -1)
-		heightInput = strings.Replace(heightInput, "\r\n", "", -1)
-	} else {
-		weightInput = strings.Replace(weightInput, "\n", "", -1)
-		heightInput = strings.Replace(heightInput, "\n", "", -1)
-	}
-
-	weight, _ = strconv.ParseFloat(weightInput, 64)
-	height, _ = strconv.ParseFloat(heightInput, 64)
+	weight = getUserInput(info.WeightPrompt)
+	height = getUserInput(info.HeightPrompt)
 	return
+}
+
+func getUserInput(promptText string) (enteredValue float64) {
+	fmt.Print(promptText)
+	// underscore means: we don't care about second output.
+	userInput, _ := reader.ReadString('\n')
+	if runtime.GOOS == "windows" {
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+	} else {
+		userInput = strings.Replace(userInput, "\n", "", -1)
+	}
+	enteredValue, _ = strconv.ParseFloat(userInput, 64)
+	return enteredValue
+
 }
