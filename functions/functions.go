55a13cc07045c663ab7d5df3aec0d48ff123e1c3
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-04 11:54:43 +0430

    Feat: New Return method.

--- functions/functions.go
+++ functions/functions.go
@@ -14,8 +14,16 @@ func main() {
 
 }
 
-func add(num1 int, num2 int) int {
-	return num1 + num2
+// func add(num1 int, num2 int) int {
+// 	return num1 + num2
+// }
+
+// another declartion of functions by creating name for each output and swiftly we can use return alonely
+// pay attention to this example
+// it will return sum without refering sum after Return keyword
+func add(num1 int, num2 int) (sum int) {
+	sum = num1 + num2
+	return
 }
 
 func printNumber(number int) {
@@ -24,6 +32,6 @@ func printNumber(number int) {
 
 func generateRandomNumbers() (int, int) {
 	randomNumber1 := rand.Intn(10)
-	randomNumber2 := rand.Intn(10)
+	randomNumber2 := rand.Intn(6)
 	return randomNumber1, randomNumber2
 }
