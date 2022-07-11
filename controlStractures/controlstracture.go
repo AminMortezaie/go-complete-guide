faa9402e660f6c99197140ec89049c51c876808c
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-11 09:06:53 +0430

    Fix: Update.

--- controlStractures/controlstracture.go
+++ controlStractures/controlstracture.go
@@ -12,10 +12,16 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Please enter your age:")
+	fmt.Printf("Please enter your age: ")
 
 	userAgeInput, _ := reader.ReadString('\n')
 	userAgeInput = strings.ReplaceAll(userAgeInput, "\r\n", "")
-	userAge, _ = strconv.ParseInt(userAgeInput, 0, 64)
+	userAge, _ := strconv.ParseInt(userAgeInput, 0, 64)
 
+	// storing a condition in a variable could be super useful and Go will approve this.
+	isOldEnough := userAge >= 18
+
+	if isOldEnough {
+		fmt.Printf("Welcome to the club!")
+	}
 }
