575ba4c7d1aba411e7f4e5dd30a75ae93fd2df56
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-11 10:13:05 +0430

    Fix: Update.

--- controlStractures/loops.go
+++ controlStractures/loops.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
+
+}
+
+func getUserChoice() (string, error) {
+	fmt.Println("Please make your choice")
+	fmt.Println("1) Add up all the numbers of to number X")
+	fmt.Println("2) Build the factorial up to number X")
+	fmt.Println("3) Sum up manually entered numbers")
+	fmt.Println("4) Sum up a list of entered numbers")
+
+	userInput, err := reader.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	userInput = strings.ReplaceAll(userInput, "\r\n", "")
+
+	if userInput == "1" ||
+		userInput == "2" ||
+		userInput == "3" ||
+		userInput == "4" {
+		return userInput, nil
+	}
 
 }
