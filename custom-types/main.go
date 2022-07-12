82026ae1f38dd6e30d79c6a31a71fd6df76b35f6
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-12 16:21:35 +0430

    Feat: Hoseini's hint.

--- custom-types/main.go
+++ custom-types/main.go
@@ -11,16 +11,29 @@ type customNumber int
 
 type personData map[string]person
 
-func (number customNumber) pow() customNumber {
-	return number * 2
+func (number customNumber) pow(power int) (result customNumber) {
+	result = number
+
+	for i := 0; i < power; i++ {
+		result = result * number
+	}
+
+	return
 }
 
 func main() {
-	var people personData = personData{
-		"p1": {
-			"amin",
-			24,
-		},
-	}
-	fmt.Printf("Print names: %v", people)
+	myVar := person{"ali", 18}
+	myVar.birthday()
+	myVar.birthday()
+	myVar.birthday()
+	myVar.birthday()
+	myVar.birthday()
+	fmt.Printf("myVar: %v\n", myVar)
+}
+
+// if you want change the value of the variable you need to declare the pointer instead of value
+// because Go would copy by value not copy by reference!
+
+func (p *person) birthday() {
+	p.age++
 }
