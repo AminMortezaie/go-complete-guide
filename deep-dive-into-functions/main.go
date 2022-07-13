af310dccd597441d19eb2c03d6082625c58fa0f1
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-13 11:54:07 +0430

    Feat: Passing a func as  parameter to a func.

--- deep-dive-into-functions/main.go
+++ deep-dive-into-functions/main.go
@@ -4,15 +4,23 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3}
-	doubled := doubledNumbers(&numbers)
+	doubled := transformNumbers(&numbers, triple)
 	fmt.Printf("doubled: %v\n", doubled)
 
 }
 
-func doubledNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 	for _, value := range *numbers {
-		dNumbers = append(dNumbers, value*2)
+		dNumbers = append(dNumbers, transform(value))
 	}
 	return dNumbers
+}
+
+func double(num int) int {
+	return num * 2
+}
+
+func triple(num int) int {
+	return num * 3
 }
