b9f262a83ce5f930f83db6227d399ddab72efbc4
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-16 15:39:24 +0430

    Fix: Update.

--- deep-dive-into-functions/main.go
+++ deep-dive-into-functions/main.go
@@ -2,17 +2,20 @@ package main
 
 import "fmt"
 
-var numbers = [...]int{1, 2, 3}
+var numbers = []int{1, 2, 3}
 
 func main() {
 
 	a := new([4]int)
 	fmt.Printf("a: %v\n", &a)
 
-	// doubled := transformNumbers(&numbers, triple)
-	// fmt.Printf("doubled: %v\n", doubled)
-	// getTransformerFunction(numbers)
-	// fmt.Printf("numbers: %v\n", numbers)
+	doubled := transformNumbers(&numbers, triple)
+	fmt.Printf("doubled: %v\n", doubled)
+	getTransformerFunction(numbers)
+	fmt.Printf("numbers: %v\n", numbers)
+
+	doubled1 := createTransformer(2)
+	fmt.Printf("doubled1: %v\n", doubled1(33))
 
 }
 
@@ -24,9 +27,9 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	return dNumbers
 }
 
-func double(num int) int {
-	return num * 2
-}
+// func double(num int) int {
+// 	return num * 2
+// }
 
 func triple(num int) int {
 	return num * 3
