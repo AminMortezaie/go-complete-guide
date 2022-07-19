88540fe7f9af32356e85bf6d9f2e6960799337d3
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-19 14:32:45 +0430

    Fix: Update.

--- interfaces/main.go
+++ interfaces/main.go
@@ -36,8 +36,37 @@ func main() {
 	message := loggableString("[INFO] User created.")
 	message.log()
 	createLog(&logData{"user logged out", "user-log.txt"})
+	outputValue(message)
+	// newNumber := []int{1, 2, 3, 4}
+	double([...]int{1, 2, 4})
 }
 
 func createLog(data logger) {
 	data.log()
+}
+
+// it accepts any type of value.
+func outputValue(value interface{}) {
+	val, ok := value.(loggableString)
+	fmt.Printf("val: %v\n", val)
+	fmt.Printf("ok: %v\n", ok)
+	fmt.Printf("value: %v \n", value)
+}
+
+func double(value interface{}) interface{} {
+	switch val := value.(type) {
+	case int:
+		return val * 2
+	case float32:
+		return val * 2
+	case []int:
+		newNum := append(val, val...)
+		fmt.Printf("newNum: %v\n", newNum)
+		return newNum
+	case [4]int:
+		fmt.Println("This is an array!")
+		return ""
+	default:
+		return ""
+	}
 }
