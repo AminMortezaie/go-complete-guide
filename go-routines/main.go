93e0a9a047532b8ddefb5855dc22c2e6bc1aedf4
aminmortezaie <mohammadmortezaie@gmail.com>
2022-07-20 09:13:05 +0430

    Feat: Using Select to find out which one is first.

--- go-routines/main.go
+++ go-routines/main.go
@@ -11,34 +11,52 @@ var randN = rand.New(source)
 
 func main() {
 
-	c := make(chan int)
+	// c := make(chan int)
+
+	x := make(chan int)
+	y := make(chan int)
+
 	// set a limiter and make channels into buffered channel.
 	// the second parameter is capacity of the buffered channel.
 	limiter := make(chan int, 3)
 
-	go generateValue(c, limiter)
-	go generateValue(c, limiter)
-	go generateValue(c, limiter)
-	go generateValue(c, limiter)
+	go generateValue(x, limiter)
+	go generateValue(y, limiter)
+	// go generateValue(c, limiter)
+	// go generateValue(c, limiter)
+
+	var a int
+	var b int
+
+	// select keyword is just like switch but just for channels.
+	// in order to find out which one is finished faster, this part of code would be helpful.
+
+	select {
+	case a = <-x:
+		fmt.Printf("X is finished first, value is %v", a)
+
+	case b = <-y:
+		fmt.Printf("Y is finished first, value is %v", b)
+	}
 
 	//getting data from channel
 	// x := <-c
 	// y := <-c
 
 	//using loop for goroutines
-	sum := 0
-	i := 0
+	// sum := 0
+	// i := 0
 
-	for num := range c {
-		sum += num
-		i++
+	// for num := range c {
+	// 	sum += num
+	// 	i++
 
-		if i == 4 {
-			close(c)
-		}
-	}
+	// 	if i == 4 {
+	// 		close(c)
+	// 	}
+	// }
 
-	fmt.Printf("sum: %v \n", sum)
+	// fmt.Printf("sum: %v \n", sum)
 }
 
 func generateValue(c chan int, limit chan int) {
